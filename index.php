5d6aebc7c8ef5342474bac91100a960e048e7049
Nimantha97 <nimanthagayan0309@gmail.com>
2021-11-17 08:13:16 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Blue Shappier Stolls </title>
+  <link rel="stylesheet" href="css/bootstrap.min.css"  />
+  <link rel="stylesheet" href="css/main.css"  />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1,user-scaleable=no"  />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" ></script>
+  <script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+  <nav class="navbar navbar-dark bg-success navbar-expand-sm">
+        <a href="" class="navbar-brand">  Blue Shappier</a>
+        <button class="navbar-toggler"data-toggle="collaps" data-target="#ccsl" ><span class="navbar-toggler-icon"></span></button>
+        <div class=" container collapse navbar-collapse justify-content-center" id="ccsl" >
+        <ul class="nav navbar-nav">
+                <!-- drop down menu  -->
+                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"> Men <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Tie</a></li>
+                    <li><a href="#">Trousers</a></li>
+                  </ul>
+                </li>
+
+
+
+
+                </li>
+                  <li class="nav-item active"> <a href="" class="nav-link">Home</a></li>
+                  <li class="nav-item"> <a href="" class="nav-link">Feachers</a></li>
+                  <li class="nav-item"> <a href="" class="nav-link">Home</a></li>
+                  <li class="nav-item"> <a href="" class="nav-link">Home</a></li>
+            </ul>
+    </div>
+  </nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+</body>
+</html>
